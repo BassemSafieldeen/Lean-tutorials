f986612a0700eea1726838484463eee63821d854
BassemSafieldeen <bassemsafieldeen@gmail.com>
2020-10-28 07:14:17 +0100

    Change Type

--- src/tut1.lean
+++ src/tut1.lean
@@ -3,14 +3,14 @@
 /--
 A type is a foo if the specified axioms are satisfied:
 -/
-class foo (α : Type*) :=
+class foo (α : Type) :=
 (axiom_1 : 4 = 4)
 (axiom_2 : 5 = 5)
 
 /--
 Some foos are also bar if an additional axiom is satisfied:
 -/
-class bar (α : Type*) extends foo α :=
+class bar (α : Type) extends foo α :=
 (axiom_3 : 6 = 6)
 
 /--
@@ -18,7 +18,7 @@ Let's prove a theorem.
 
 Theorem: If a type is bar it is also foo.
 -/
-theorem foo_of_bar (α : Type*) : bar α → foo α :=
+theorem foo_of_bar (α : Type) : bar α → foo α :=
 begin
     intro,
     split,
@@ -29,7 +29,7 @@ end
 /--
 Let's prove another theorem: If a type α is foo then 4 = 4.
 -/
-theorem four_eq_four_of_foo (α : Type*) : foo α → 4 = 4 := 
+theorem four_eq_four_of_foo (α : Type) : foo α → 4 = 4 := 
 begin
     intro,
     exact a.axiom_1,
