1a9f1bcbcb46c3ef32e75bc93350ca095fbc52c0
BassemSafieldeen <bassemsafieldeen@gmail.com>
2020-10-31 09:19:00 +0100

    Add tut3

---
+++ src/tut3.lean
@@ -0,0 +1,38 @@
+import tut2
+
+import linear_algebra.tensor_product
+
+open_locale tensor_product
+
+variables {ℋ : Type} [complex_hilbert_space ℋ]
+{ρ σ τ : ℋ →ₗ[ℂ] ℋ} [quantum_state ρ] [quantum_state σ] [quantum_state τ]
+
+#check tensor_product
+#check ρ ⊗ₜ[ℂ] σ
+
+notation A ` ⊗ ` B := A ⊗ₜ[ℂ] B
+
+example {a : ℂ} : a • (ρ ⊗ σ) = (a • ρ) ⊗ σ :=
+begin
+    exact rfl,
+end
+
+example {a : ℂ} : a • (ρ ⊗ σ) = ρ ⊗ (a • σ) :=
+begin
+    norm_num,
+end
+
+example {a : ℂ} : a • (ρ + σ) = a • ρ + a • σ := 
+begin
+    rw smul_add,
+end
+
+example : ρ ⊗ (σ + τ) = ρ ⊗ σ + ρ ⊗ τ := 
+begin
+    rw tensor_product.tmul_add,
+end 
+
+example : (σ + τ) ⊗ ρ = σ ⊗ ρ + τ ⊗ ρ := 
+begin
+    rw tensor_product.add_tmul,
+end
