031a14fa9700898e9895c9d41be8495275618c46
BassemSafieldeen <bassemsafieldeen@gmail.com>
2020-11-15 17:51:58 +0100

    Update to use latest version of Shannon_theory

--- src/tut28_entropy.lean
+++ src/tut28_entropy.lean
@@ -1,5 +1,6 @@
 import linear_algebra.eigenspace
 import linear_algebra.tensor_product
+import rnd_var
 import shannon_theory
 
 import tut3
@@ -7,6 +8,8 @@ import tut5
 -- import tut13_ptrace
 
 ---- QUANTUM ENTROPY
+
+open rnd_var
 
 open_locale tensor_product big_operators
 
@@ -16,6 +19,12 @@ variables
 {ℋ : Type} [complex_hilbert_space ℋ]
 {ℋ₁ : Type} [complex_hilbert_space ℋ₁]
 {ℋ₂ : Type} [complex_hilbert_space ℋ₂]
+
+universe x
+
+variables 
+{ι : Type x} -- indexing type
+[fintype ι] -- tell Lean that the set of all elements ι is finite.
 
 /-
 Logarithm of a linear operator.
@@ -32,9 +41,9 @@ universe x
 /--
 If a linear operator has trace one then its eigenvalues sum to 1.
 -/
-lemma sum_eigenvalues_eq_one_of_trace_one (ι : Type x) (ρ : module.End ℂ ℋ) {s : finset ι}
-{e : ι → ℝ} {he : ∀ i ∈ s, ρ.has_eigenvalue (e i)}: 
-(Tr ρ = 1) → (∑ i in s, e i = 1) := 
+lemma sum_eigenvalues_eq_one_of_trace_one (ρ : module.End ℂ ℋ) 
+{e : ι → ℝ} {he : ∀ i, ρ.has_eigenvalue (e i)}: 
+(Tr ρ = 1) → (∑ i, e i = 1) := 
 begin
     sorry
 end
@@ -49,45 +58,41 @@ lemma quantum_state_pos_semidef (ρ : module.End ℂ ℋ) [quantum_state ρ] (e 
 /--
 Quantum entropy is nonnegative
 -/
-theorem entropy_nonneg (ι : Type x) {ρ : module.End ℂ ℋ} {s : finset ι}
-{e : ι → ℝ} {he : ∀ i ∈ s, ρ.has_eigenvalue (e i)} [quantum_state ρ] : entropy ρ ≥ 0 := 
+theorem entropy_nonneg  {ρ : module.End ℂ ℋ}
+{e : ι → ℝ} {he : ∀ i, ρ.has_eigenvalue (e i)} [quantum_state ρ] : 
+entropy ρ ≥ 0 := 
 begin
-    have : entropy ρ = - ∑ i in s, (e i) * real.log(e i), {sorry},
+    have : entropy ρ = - ∑ i, (e i) * real.log(e i), {sorry},
     rw this,
     clear this,
     -- All the eignevalues are nonnegative because the state is positive semidefinite
-    have he1 : ∀ i ∈ s, (e i) ≥ 0, {
+    have he_nonneg : ∀ i, (e i) ≥ 0, {
         intros,
         apply quantum_state_pos_semidef ρ,
-        exact he i H,
+        exact he i,
     },
     -- The eigenvalues sum to 1 because the state has trace 1.
-    have he2 : ∑ i in s, (e i) = 1, {
-        apply sum_eigenvalues_eq_one_of_trace_one ι ρ quantum_state.trace_one,
-        exact he,
+    have he_sum_eq_one : ∑ i, (e i) = 1, {
+        apply sum_eigenvalues_eq_one_of_trace_one ρ quantum_state.trace_one,
+        apply he,
     },
-    -- The remaining term looks Shannon entropy, so we use that Shannon 
-    -- entropy is nonegative to finish the proof.
-    have he3 : Shannon_entropy s e he1 he2 ≥ 0, by exact Shannon_entropy_nonneg s e,
-    rw Shannon_entropy at he3,
-    exact he3,
+    -- Given the available facts, the remaining term looks like Shannon 
+    -- entropy. So we use that Shannon entropy is nonegative to finish.
+    apply Shannon_entropy_nonneg, simp,
+    {exact he_nonneg},
+    {exact he_sum_eq_one},
 end
-
-def rnd_var {ι : Type x} (s : finset ι) (pₓ : ι → ℝ) :=
-(∀ i ∈ s, pₓ i ≥ 0) ∧ (∑ i in s, pₓ i = 1)
 
 /--
 Convacity of the entropy
 -/
-theorem entropy_concave 
-(ι : Type x) {sₓ : finset ι} {ρ : module.End ℂ ℋ}
-{sₑ : finset ι} {e : ι → ℝ} {he : ∀ i ∈ sₑ, ρ.has_eigenvalue (e i)}
-(pₓ : ι → ℝ) [hpₓ : rnd_var sₓ pₓ] (ρₓ : ι → module.End ℂ ℋ)
-(hρ : ρ = ∑ i in sₓ, (pₓ i) • (ρₓ i)) : 
-entropy ρ ≥ ∑ i in sₓ, (pₓ i) * entropy (ρₓ i) := 
+theorem entropy_concave  {ρ : module.End ℂ ℋ} 
+{e : ι → ℝ} {he : ∀ i, ρ.has_eigenvalue (e i)}
+(pₓ : ι → ℝ) [hpₓ : rnd_var pₓ] (ρₓ : ι → module.End ℂ ℋ)
+(hρ : ρ = ∑ i, (pₓ i) • (ρₓ i)) : 
+entropy ρ ≥ ∑ i, (pₓ i) * entropy (ρₓ i) := 
 begin
-    rw rnd_var at hpₓ,
-    have : entropy ρ = - ∑ i in sₑ, (e i) * real.log(e i), {sorry},
+    have : entropy ρ = - ∑ i, (e i) * real.log(e i), {sorry},
     rw this,
     clear this,
     sorry
